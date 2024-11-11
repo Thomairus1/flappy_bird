56dec1fea20d17745be79d2f73edacb72a4d92d7
Thomairus1 <thomas.rugeles@ucas-edu.net>
2024-11-11 11:50:54 -0700

    updated bird

--- game.py
+++ game.py
@@ -13,6 +13,8 @@ class Game:
         self.gravity = 0.05
         self.bird_movement = 0
         self.rotated_bird = pygame.Surface((0,0))
+        self.pipes= []
+        self.pipe_height = [280, 425, 562]
 
     def resize_images(self):
         self.bird = pygame.transform.scale(self.bird, (51,34))
@@ -46,3 +48,33 @@ class Game:
     def flap(self):
         self.bird_movement = 0
         self.bird_movement -= 2.5
+
+    def add_pipe(self):
+        random_pipe_pos = random.choice(self.pipe_height)
+        bottom_pipe = self.pipe.get_rect(midtop = (600, random_pipe_pos))
+        top_pipe = self.pipe.get_rect(midbottom = (600, random_pipe_pos-211))
+        self.pipes.append(bottom_pipe)
+        self.pipes.append(top_pipe)
+
+    def move_pipes(self):
+        for pipe in self.pipes:
+            pipe.centerx -= 1
+            if pipe.centerx <= -40:
+                self.pipes.remove(pipe)
+
+    def show_pipes(self, screen):
+         for pipe in self.pipes:
+            if pipe.bottom >= 700:
+                 screen.blit(self.pipe, pipe)
+            else:
+                flip_pipe = pygame.transform.flip(self.pipe, False, True)
+                screen.blit(flip_pipe, pipe)
+
+            
+    def check_collision(self):
+        for pipe in self.pipes:
+            if self.bird_erect.colliderect(pipe):
+                self.active = False
+
+        if self.bird_rect.top <= -100 or self.bird_rect.bottom >= 650:
+            self.active = False
